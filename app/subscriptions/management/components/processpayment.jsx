13cd9d8c3d334e841190f94919e9939d86ee64fc
Hammad Syed <hammadword08@gmail.com>
2025-04-17 12:43:46 +0500

    main

---
+++ app/subscriptions/management/components/processpayment.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import styles from '../management.module.css'
+
+const ProcessPayment = () => {
+  const PaymentData = [
+    {
+
+    }
+  ]
+  // qwertyui
+  return (
+    <div>
+      <div className={styles.searchBar}>
+        <input className={styles.Input} type="text" placeholder='Search payment..' />
+      </div>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th></th>
+            </tr>
+          </thead>
+        </table>
+      </div>
+    </div>
+  )
+}
+
+export default ProcessPayment
